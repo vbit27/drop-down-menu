90fcb59c4b066c36f4a45c871471b9cfe5aa5663
Vasil Bit <vasil.bituni@gmail.com>
2021-04-06 16:03:13 +0200

    Add toggle class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const menuItems = document.querySelectorAll('.main-menu');
 const dropMenu = document.querySelectorAll('.drop-menu');
 
-function display() {
-  this.querySelector('ul').setAttribute('display', 'flex');
-}
+/*
+
 
 function hide() {
   this.querySelector('ul').setAttribute('display', 'none');
 }
 
-menuItems.forEach((item) => {
-  item.addEventListener('mouseover', display);
-});
+
 
 menuItems.forEach((item) => {
   item.addEventListener('mouseout', hide);
+});
+*/
+function display() {
+  this.querySelector('ul').classList.toggle('visible');
+}
+
+menuItems.forEach((item) => {
+  item.addEventListener('mouseover', display);
 });
